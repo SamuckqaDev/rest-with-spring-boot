e1f9115b86843c61b9ee07a5a4f3b71620d2de21
samuckqadev <samuel.tomimatu@bandtec.com.br>
2023-03-28 15:57:23 -0300

    Settings authentication service

---
+++ src/main/java/br/com/matsutech/restwithspringbootjava/controller/AuthController.java
@@ -0,0 +1,52 @@
+package br.com.matsutech.restwithspringbootjava.controller;
+
+import br.com.matsutech.restwithspringbootjava.data.vo.v1.AccountCredentialsVO;
+import br.com.matsutech.restwithspringbootjava.services.AuthServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Authentication End-point")
+@RestController
+@RequestMapping("/auth")
+public class AuthController implements AuthenticationManager {
+
+    @Autowired
+    private AuthServices authServices;
+
+    @Operation(summary = "Authenticates a user and returns a token")
+    @PostMapping(value = "/singIn")
+    public ResponseEntity singIn(@RequestBody AccountCredentialsVO data) throws Exception {
+        if (checkIsBlankAndNotNull(data)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request ");
+        }
+
+        var token = authServices.signIn(data);
+
+        if (token == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request ");
+        }
+
+        return token;
+    }
+
+    private static boolean checkIsBlankAndNotNull(AccountCredentialsVO data) {
+        return data == null || data.getUsername() == null || data.getUsername().isBlank()
+                || data.getPassword() == null || data.getUsername().isBlank() || data.getPassword().isBlank();
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+}
